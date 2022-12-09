1d10ef854ed04b9d471071c0510b650ab9e90f0f
shatabdi-pal <shatabdi@pdx.edu>
2022-12-08 20:33:50 -0800

    grid issue fix

--- pygame_grid.py
+++ pygame_grid.py
@@ -81,9 +81,6 @@ def is_terminal_state(current_row_index, current_column_index):
   else:
     return True
 
-
-
-
 def get_starting_location():
   current_row_index = np.random.randint(environment_rows)
   current_column_index = np.random.randint(environment_columns)
@@ -103,16 +100,18 @@ ant = Ant()
 def get_next_location(current_row_index, current_column_index, action_index):
   new_row_index = current_row_index
   new_column_index = current_column_index
-  lslsl
+  if actions[action_index] == 'up' and current_row_index > 0 and ant.ant_y>0:
+        new_row_index -= 1
+        ant.ant_y -= vel
   elif actions[action_index] == 'right' and current_column_index < environment_columns - 1 and ant.ant_x <ant.window_x - ant.ant_size:
-    new_column_index += 1
-    ant.ant_x += vel
+        new_column_index += 1
+        ant.ant_x += vel
   elif actions[action_index] == 'down' and current_row_index < environment_rows - 1  and ant.ant_y < ant.window_y - ant.ant_size:
-    new_row_index += 1
-    ant.ant_y += vel
+        new_row_index += 1
+        ant.ant_y += vel
   elif actions[action_index] == 'left' and current_column_index > 0 and ant.ant_x > 0:
-    new_column_index -= 1
-    ant.ant_x -= vel
+        new_column_index -= 1
+        ant.ant_x -= vel
   return new_row_index, new_column_index
 
 
@@ -133,22 +132,7 @@ def get_shortest_path(start_row_index, start_column_index):
 epsilon = 0.9
 discount_factor = 0.9
 learning_rate = 0.2
-for episode in range(1000):
-    row_index, column_index = get_starting_location()
 
-    while not is_terminal_state(row_index, column_index):
-        action_index = get_next_action(row_index, column_index, epsilon)
-
-        old_row_index, old_column_index = row_index, column_index
-        row_index, column_index = get_next_location(row_index, column_index, action_index)
-
-        reward = rewards[row_index, column_index]
-        old_q_value = q_values[old_row_index, old_column_index, action_index]
-        temporal_difference = reward + (discount_factor * np.max(q_values[row_index, column_index])) - old_q_value
-
-        new_q_value = old_q_value + (learning_rate * temporal_difference)
-        q_values[old_row_index, old_column_index, action_index] = new_q_value
-# print('Training complete!')
 
 #ant = Ant()
 running = True
@@ -157,6 +141,22 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    for episode in range(10):
+        row_index, column_index = get_starting_location()
+
+        while not is_terminal_state(row_index, column_index):
+            action_index = get_next_action(row_index, column_index, epsilon)
+
+            old_row_index, old_column_index = row_index, column_index
+            row_index, column_index = get_next_location(row_index, column_index, action_index)
+
+            reward = rewards[row_index, column_index]
+            old_q_value = q_values[old_row_index, old_column_index, action_index]
+            temporal_difference = reward + (discount_factor * np.max(q_values[row_index, column_index])) - old_q_value
+
+            new_q_value = old_q_value + (learning_rate * temporal_difference)
+            q_values[old_row_index, old_column_index, action_index] = new_q_value
+#print('Training complete!')
 
     ant.display_ant()
     ant.display_wall()
@@ -167,8 +167,7 @@ while running:
 pygame.quit()
 
 
-#print(get_shortest_path(3, 8))
-print(get_shortest_path(5, 5))
+print(get_shortest_path(3, 8))
+#print(get_shortest_path(5, 5))
 #print(get_shortest_path(8, 5))
-
 
